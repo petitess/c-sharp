bd74a25576c071b0303339e836929eeb9891e27e
Karol <93782725+petitess@users.noreply.github.com>
2023-10-24 17:41:51 +0200

    methods

---
+++ methods01.cs
@@ -0,0 +1,16 @@
+namespace SimpleMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            Console.ReadLine();
+        }
+        private static void HelloWorld()
+        { 
+            Console.WriteLine("Hello world");
+        }
+
+    }
+}

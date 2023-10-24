a0d0a0293f53f510165b0d388effb3169cc38293
Karol <93782725+petitess@users.noreply.github.com>
2023-10-24 18:48:16 +0200

    Update methods01.cs

--- methods01.cs
+++ methods01.cs
@@ -40,3 +40,36 @@ namespace SimpleMethod
 
     }
 }
+//////////////////
+namespace SimpleMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your first name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("What's your last name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("In what city you were born");
+            string city = Console.ReadLine();
+
+
+            Console.Write("Results: ");
+            string reversedFirstName = ReverseString(firstName);
+            string reversedLastName = ReverseString(lastName);
+            string reversedCity = ReverseString(city);
+
+            Console.Write(String.Format("{0} {1} {2}", 
+                reversedFirstName, 
+                reversedLastName, 
+                reversedCity));
+        }
+        private static string ReverseString(string message)
+        {
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            return String.Concat(messageArray);
+        }
+    }
+}

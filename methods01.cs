fd449226245d32f66cbae9e59d2a7f3cff877a6b
Karol <93782725+petitess@users.noreply.github.com>
2023-10-24 18:12:32 +0200

    Update methods01.cs

--- methods01.cs
+++ methods01.cs
@@ -14,3 +14,29 @@ namespace SimpleMethod
 
     }
 }
+//////////////////
+namespace SimpleMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            ReverseString(Console.ReadLine());
+             
+            Console.WriteLine("What's your dogs name?");
+            ReverseString(Console.ReadLine());
+        }
+        private static void ReverseString(string message)
+        {
+            char[] firstNameArray = message.ToCharArray();
+            Array.Reverse(firstNameArray);
+            foreach (char item in firstNameArray)
+            {
+                Console.Write(item);
+            }
+            Console.Write(" ");
+        }
+
+    }
+}

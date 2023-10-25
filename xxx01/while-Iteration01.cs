c06acb17afd6dbbeb93e4fe11fa3af9dd41214d7
Karol <93782725+petitess@users.noreply.github.com>
2023-10-25 17:46:05 +0200

    Create while-Iteration01.cs

---
+++ xxx01/while-Iteration01.cs
@@ -0,0 +1,50 @@
+namespace SimpleMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+                displayMenu = MainMenu();
+            }
+        }
+        private static bool MainMenu()
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Option 1");
+            Console.WriteLine("2) Option 2");
+            Console.WriteLine("3) Exit");
+            string results = Console.ReadLine();
+            if(results == "1")
+            {
+                PrintNumbers();
+                return true;
+            }
+            else if (results == "2")
+            {
+                GuessingGame();
+                return true;
+            }
+            else if (results == "3")
+            {
+                return false;
+            }
+            else 
+            { 
+                return true;
+            }
+        }
+
+        private static void PrintNumbers()
+        {
+            Console.WriteLine("Print numbers!");
+        }
+
+        private static void GuessingGame() 
+        {
+            Console.WriteLine("Guessing game!");
+        }
+    }
+}

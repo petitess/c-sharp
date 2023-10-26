fff84e3f9c93ebd3fb648fe78586fcf5422cade4
Karol <93782725+petitess@users.noreply.github.com>
2023-10-26 08:08:48 +0200

    Update formatString01.cs

--- xxx01/formatString01.cs
+++ xxx01/formatString01.cs
@@ -31,6 +31,36 @@ namespace SimpleMethod
             string format5 = String.Format("Phone Number: {0:(###) ###-####}", 1234567890);
             Console.WriteLine(format5);
 
+            string lyric = " That summer we took threes across the board ";
+            lyric = lyric.Substring(6,18);
+            Console.WriteLine(lyric);
+            lyric = lyric.ToUpper();
+            Console.WriteLine(lyric);
+            lyric = lyric.Replace(" ", "-");
+            Console.WriteLine(lyric);
+            lyric = lyric.Remove(2, 8);
+            Console.WriteLine(lyric);
+
+            string lyric2 = " When I met you in the summer. To my heartbeat's sound. We fell in love. As the leaves turned brown ";
+            lyric2 = String.Format("Length befor: {0} -- Length after: {1}", 
+                lyric2.Length, 
+                lyric2.Trim().Length);
+            Console.WriteLine(lyric2);
+
+            string concat = "";
+            for (int i = 0; i < 100; i++)
+            {
+                concat += "--" + i.ToString();
+            }
+            Console.WriteLine(concat);
+
+            StringBuilder stringx = new StringBuilder();
+            for (int i = 0; i < 100; i++)
+            {
+                stringx.Append("-");
+                stringx.Append(i);
+            }
+            Console.WriteLine(stringx);
         }
     }
 }

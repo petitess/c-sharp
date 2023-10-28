64cc8f4fa6691db90dfb1ecd60df23de8cc91f51
Karol <93782725+petitess@users.noreply.github.com>
2023-10-28 13:39:55 +0200

    Create dataTypes01.cs

---
+++ xxx01/dataTypes01.cs
@@ -0,0 +1,5 @@
+//String
+string[] name = { "Ann", "Bob", "Carl", "David" };
+//Object
+Car toyota = new Car() {Make = "Toyota", Model = "Land Cruiser", VIN = "ASD456"};
+Car dodge = new Car() { Make = "Dodge", Model = "Charger", VIN = "POI098" };

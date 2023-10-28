4acccff75e8ea1a99e51ffdb1a240a34eed7b16f
Karol <93782725+petitess@users.noreply.github.com>
2023-10-28 13:48:14 +0200

    Update dataTypes01.cs

--- xxx01/dataTypes01.cs
+++ xxx01/dataTypes01.cs
@@ -1,5 +1,10 @@
 //String
 string[] name = { "Ann", "Bob", "Carl", "David" };
-//Object
+//Object initalizer. No need for constractor.
 Car toyota = new Car() {Make = "Toyota", Model = "Land Cruiser", VIN = "ASD456"};
 Car dodge = new Car() { Make = "Dodge", Model = "Charger", VIN = "POI098" };
+//Collection initializer
+List<Car> list = new List<Car>() {
+    new Car {Make = "Toyota", Model = "Land Cruiser", VIN = "ASD456"},
+    new Car {Make = "Dodge", Model = "Charger", VIN = "POI098" },
+};

9b57d995ec9beaa438b508972c13012cd7a90dff
Karol <93782725+petitess@users.noreply.github.com>
2023-10-29 17:03:36 +0100

    Guessing game

--- xxx01/methods03.cs
+++ xxx01/methods03.cs
@@ -4,41 +4,74 @@ namespace SimpleMethod
     {
         static void Main(string[] args)
         {
-
-            MainMenu();
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+                displayMenu = MainMenu();
+            }
         }
-
-        private static void MainMenu()
+        private static bool MainMenu()
         {
+            Console.Clear();
             Console.WriteLine("Choose an option:");
             Console.WriteLine("1) Option 1");
             Console.WriteLine("2) Option 2");
             Console.WriteLine("3) Exit");
             string results = Console.ReadLine();
-            if(results == "1")
+            if (results == "1")
             {
                 PrintNumbers();
+                return true;
             }
             else if (results == "2")
             {
                 GuessingGame();
+                return true;
             }
             else if (results == "3")
             {
-
+                return false;
+            }
+            else
+            {
+                return true;
             }
         }
-
         private static void PrintNumbers()
         {
+            Console.Clear();
             Console.WriteLine("Print numbers!");
+            Console.Write("Type a number; ");
+            int result = int.Parse(Console.ReadLine());
+            int counter = 1;
+            while (counter < result + 1)
+            {
+                Console.Write(counter);
+                Console.Write("-");
+                counter++;
+            }
+            Console.ReadLine();
         }
-
-        private static void GuessingGame() 
+        private static void GuessingGame()
         {
+            Console.Clear();
             Console.WriteLine("Guessing game!");
+            Random random = new Random();
+            int randomNumber = random.Next(1, 11);
+            int guesses = 0;
+            bool incorrect = true;
+            do
+            {
+                Console.WriteLine("Guess a number between 1 and 10; ");
+                string result = Console.ReadLine();
+                guesses++;
+                if (result == randomNumber.ToString())
+                    incorrect = false;
+                else
+                    Console.WriteLine("Wrong!");
+            } while (incorrect);
+            Console.WriteLine("Correct! It took you {0} guesses.", guesses);
+            Console.ReadLine();
         }
-
-
     }
 }

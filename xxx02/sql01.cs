9847d8d88d8942ac5c5326f0106346180438ab23
Karol <93782725+petitess@users.noreply.github.com>
2023-10-30 18:19:20 +0100

    run SQL query in c#

--- xxx02/sql01.cs
+++ xxx02/sql01.cs
@@ -4,7 +4,7 @@ IDbConnection dbConnection = new SqlConnection(connection);
 string sqlCommand = "SELECT GETDATE()";
 DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
 Console.WriteLine(rightNow.ToString()); 
-//Write data to database
+//Write data to a database
 string connection = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true" ;
 IDbConnection dbConnection = new SqlConnection(connection);
 string sql = @"INSERT INTO TutorialAppSchema.Computer (
@@ -23,3 +23,26 @@ string sql = @"INSERT INTO TutorialAppSchema.Computer (
 Console.WriteLine(sql);
 int result = dbConnection.Execute(sql);
 Console.WriteLine(result);
+//Get data from a database
+string connection = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true" ;
+IDbConnection dbConnection = new SqlConnection(connection);
+string sqlSelect = @"SELECT 
+    Computer.Motherboard,
+    Computer.CPUCores,
+    Computer.HasWifi,
+    Computer.HasLTE,
+    Computer.VideoCard 
+    FROM TutorialAppSchema.Computer";
+
+IEnumerable<Computer.Models.Computer> computers = dbConnection.Query<Computer.Models.Computer>(sqlSelect);
+Console.WriteLine("'Motherboard', 'HasWifi', 'HasLTE', 'ReleaseDate',"
+    + ", 'Prise', 'VideoCard'");
+foreach(Computer.Models.Computer comp in computers)
+{
+    Console.WriteLine("'" + myComputer.Motherboard 
+         + "','" + myComputer.CPUCores
+         + "','" + myComputer.HasWifi
+         + "','" + myComputer.HasLTE
+         + "','" + myComputer.VideoCard
+         + "'");
+}

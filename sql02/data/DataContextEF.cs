972428452a685aebebbb8a683cd0a3f4b831abd1
Karol <93782725+petitess@users.noreply.github.com>
2023-10-31 18:15:02 +0100

    insert data into a database - Entity Framework

---
+++ sql02/data/DataContextEF.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Entity.data
+{
+    //DataContextEF : DbContext = DataContextEF inherits DbContext
+    public class DataContextEF : DbContext
+    {
+        public DbSet<Computer.Models.Computer>? Computer { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer("Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true;",
+                    option => option.EnableRetryOnFailure());
+
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Computer.Models.Computer>()
+                .ToTable("Computer", "TutorialAppSchema").HasKey(c => c.ComputerId);
+        }
+
+    }
+}

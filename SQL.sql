5a364954cac4f238e26e933084cff5987284963f
Karol <93782725+petitess@users.noreply.github.com>
2023-11-01 03:04:51 +0100

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -1,3 +1,4 @@
+--Create a database
 CREATE DATABASE DotNetCourseDatabase
 GO
 
@@ -19,3 +20,13 @@ CREATE TABLE TutorialAppSchema.Computer(
 );
 
 SELECT * FROM TutorialAppSchema.Computer
+-----------------------
+--Create a login to SQL based on Windows account
+CREATE LOGIN [<domainName>\<loginName>] FROM WINDOWS;
+CREATE LOGIN [DESKTOP-CFCPFSG\JOBB] FROM WINDOWS;
+--Grant reader permission
+USE DotNetCourseDatabase
+EXEC sp_addrolemember 'db_datareader', 'DESKTOP-CFCPFSG\JOBB';
+--Grant owner permission
+use DotNetCourseDatabase
+exec sp_addrolemember 'db_owner', 'DESKTOP-CFCPFSG\JOBB';

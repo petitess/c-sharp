a13a8b9262c1952fa3400d89bbae710b5a2039af
Karol <93782725+petitess@users.noreply.github.com>
2023-11-04 14:17:33 +0100

    map JSON data - Entity Framework

---
+++ automapper02/Data/DataContextEF.cs
@@ -0,0 +1,42 @@
+﻿using DotnetAPI.Dtos;
+using DotnetAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotnetAPI.Data
+{
+    public class DataContextEF : DbContext
+    {
+        private readonly IConfiguration _config;
+        public DataContextEF(IConfiguration config)
+        {
+            _config = config;
+        }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<UserSalary> UsersSalary { get; set; }
+        public virtual DbSet<UserJobInfo> UsersJobInfo { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_config.GetConnectionString("DefaultConnection"),
+                    optionsBuilder => optionsBuilder.EnableRetryOnFailure());
+            }
+        }
+        //void isn't returning anything
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("TutorialAppSchema");
+
+            modelBuilder.Entity<User>()
+                //Tell the User-model that the tabled inside database is called Users
+                .ToTable("Users", "TutorialAppSchema")
+                .HasKey(u => u.UserId);
+
+            modelBuilder.Entity<UserSalary>()
+                .HasKey(u => u.UserId);
+
+            modelBuilder.Entity<UserJobInfo>()
+                .HasKey(u => u.UserId);
+        }
+    }
+}

6b3213f71dd838af612bff7da695673fff87f7c1
Karol <93782725+petitess@users.noreply.github.com>
2023-11-05 08:38:33 +0100

    CORS settings

---
+++ xxx02/cors01.cs
@@ -0,0 +1,31 @@
+var app = builder.Build();
+
+builder.Services.AddCors((options =>
+{
+    options.AddPolicy("DevCors", (corsBuilder) =>
+    {
+        corsBuilder.WithOrigins("http://localhost:4200", "http://localhost:3000", "http://localhost:8000")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+    });
+    options.AddPolicy("ProdCors", (corsBuilder) =>
+    {
+        corsBuilder.WithOrigins("http://myproductionsite.com")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+    });
+}));
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors("DevCors");
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseCors("ProdCors");
+    app.UseHttpsRedirection();
+}

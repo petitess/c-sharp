89e38466d5dd0c276de0e008c9738d70925b9c16
Karol <93782725+petitess@users.noreply.github.com>
2023-11-10 12:21:22 +0100

    Delete controller01 directory

--- controller01/Models/UserSalary.cs
+++
@@ -1,9 +0,0 @@
-namespace DotnetAPI2.Models
-{
-    public class UserSalary
-    {
-        public int UserId { get; set; }
-        public decimal Salary { get; set; }
-        public decimal AvgSalary { get; set; }
-    }
-}

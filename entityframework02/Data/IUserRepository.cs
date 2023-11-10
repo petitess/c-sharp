0560f9ce602d66f2633990f8c8c720cbc96baa77
Karol <93782725+petitess@users.noreply.github.com>
2023-11-10 09:35:12 +0100

    endpoints to modify database - UserRepository

---
+++ entityframework02/Data/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using DotnetAPI.Models;
+
+namespace DotnetAPI_EF.Data
+{
+    public interface IUserRepository
+    {
+        public bool SaveChanges();
+        public void AddEntity<T>(T entityToAdd);
+        public void RemoveEntity<T>(T entityToRemove);
+        public IEnumerable<User> GetUsers();
+        public User GetSingleUser(int userId);
+        public IEnumerable<UserJobInfo> GetJobInfo();
+        public UserJobInfo GetSingleJobInfo(int userId);
+        public IEnumerable<UserSalary> GetSalary();
+        public UserSalary GetSingleSalary(int userId);
+    }
+}

3ec867696310ea70b0785473e5f044cfd7318af6
Karol <93782725+petitess@users.noreply.github.com>
2023-11-10 12:20:59 +0100

    configuration file to connect to a SQL server

---
+++ API/configfile01/data/DataContextDapper.cs
@@ -0,0 +1,44 @@
+﻿
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DapperX.data
+{
+    internal class DataContextDapper
+    {
+        private IConfiguration _config;
+        private string? _connection;
+        public DataContextDapper(IConfiguration config)
+        {
+            _config = config;
+            _connection = config.GetConnectionString("DefaultConnection");
+        }
+        public IEnumerable<T> LoadData<T>(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_connection);
+            return dbConnection.Query<T>(sql);
+        }
+
+        public T LoadDataSingle<T>(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_connection);
+            return dbConnection.QuerySingle<T>(sql);
+        }
+
+        public bool ExecuteSql(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_connection);
+            return (dbConnection.Execute(sql) > 0 );
+        }
+
+        public int ExecuteSqlWithRowCount(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_connection);
+            return dbConnection.Execute(sql);
+        }
+
+    }
+}
+

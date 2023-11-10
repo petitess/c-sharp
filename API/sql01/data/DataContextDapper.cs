0159a16b01669359284abba52f854d8415f2c624
Karol <93782725+petitess@users.noreply.github.com>
2023-11-10 12:19:13 +0100

    insert data into a database - dapper

---
+++ API/sql01/data/DataContextDapper.cs
@@ -0,0 +1,37 @@
+﻿
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DapperX.data
+{
+    internal class DataContextDapper
+    {
+        private string _connection = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true";
+        public IEnumerable<T> LoadData<T>(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_connection);
+            return dbConnection.Query<T>(sql);
+        }
+
+        public T LoadDataSingle<T>(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_connection);
+            return dbConnection.QuerySingle<T>(sql);
+        }
+
+        public bool ExecuteSql(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_connection);
+            return (dbConnection.Execute(sql) > 0);
+        }
+
+        public int ExecuteSqlWithRowCount(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_connection);
+            return dbConnection.Execute(sql);
+        }
+
+    }
+}
+

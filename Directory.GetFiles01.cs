f1e72c1b1428c6c767dcc9628ba2f8bac64712e2
Karol <93782725+petitess@users.noreply.github.com>
2023-11-11 17:30:20 +0100

    list files in a folder

--- Directory.GetFiles01.cs
+++ Directory.GetFiles01.cs
@@ -2,3 +2,11 @@ string path = "D:\\MUZ";
 string[] files = Directory.GetFiles(path);
 foreach (string file in files)
 Console.WriteLine(file.Replace(path + "\\", ""));
+///////
+string path = "D:\\MUZ";
+string[] files = Directory.GetFiles(path);
+foreach (string file in files)
+{
+    FileInfo fileInfo = new FileInfo(file);
+    Console.WriteLine(fileInfo.Name);
+}

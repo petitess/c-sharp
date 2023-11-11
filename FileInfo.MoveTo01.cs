70e911fe59d4f86941a302799917b8448c2b7e54
Karol <93782725+petitess@users.noreply.github.com>
2023-11-11 17:58:22 +0100

    rename files

--- FileInfo.MoveTo01.cs
+++ FileInfo.MoveTo01.cs
@@ -6,3 +6,12 @@ foreach (string file in files)
     FileInfo fileInfo = new FileInfo(file);
     fileInfo.MoveTo(path + "\\" + nr++ + "." + fileInfo.Name);
 }
+////Number with leading eros
+string path = "D:\\MUZ";
+string[] files = Directory.GetFiles(path);
+int nr = 1;
+foreach (string file in files)
+{
+    FileInfo fileInfo = new FileInfo(file);
+    fileInfo.MoveTo(path + "\\" + nr++.ToString("D3") + "." + fileInfo.Name);
+}

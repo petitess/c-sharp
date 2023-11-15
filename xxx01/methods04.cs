89cd630df14d91712a49e97f4a338b10f7d07189
Karol <93782725+petitess@users.noreply.github.com>
2023-11-15 18:11:52 +0100

    Create methods04.cs

---
+++ xxx01/methods04.cs
@@ -0,0 +1,19 @@
+static void GetScore()
+{
+    int hightscore = 55;
+    Console.WriteLine("Whats you name?");
+    string? name = Console.ReadLine();
+    Console.WriteLine("Whats your score?");
+    string? score = Console.ReadLine();
+    int scoreToInt = int.Parse(score);
+
+    if (scoreToInt > hightscore)
+    {
+        Console.WriteLine("New hightscore is " + scoreToInt);
+        hightscore = scoreToInt;
+    }
+    else
+    {
+        Console.WriteLine("Try again");
+    }
+}

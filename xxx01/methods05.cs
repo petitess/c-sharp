f10d1dc110644e72f7b58de1e5c07d13d69c6f83
Karol <93782725+petitess@users.noreply.github.com>
2023-11-17 04:40:15 +0100

    Update methods05.cs

--- xxx01/methods05.cs
+++ xxx01/methods05.cs
@@ -1,18 +1,25 @@
 //Program.cs
-Car audi = new Car("Audi");
+Car audi = new Car("Audi", "400", "black");
 audi.Drive();
 audi.Stop();
+audi.Info();
 //Car.cs
-  namespace exercise01
+namespace exercise01
 {
     internal class Car
     {
+        //Member variable/private filed
         private string _name; 
-        public Car(string name) 
+        private string _hp;
+        private string _color;
+        public Car(string name,string hp, string color) 
         {
             _name = name;
+            _hp = hp;
+            _color = color;
             Console.WriteLine(name + " was created");
         }
+        //Member method
         public void Drive()
         {
             Console.WriteLine(_name + " is driving");
@@ -20,6 +27,10 @@ audi.Stop();
         public void Stop() 
         {
             Console.WriteLine(_name + " stopped");
+        }
+        public void Info()
+        {
+            Console.WriteLine(_name + " has " + _hp + " horsepower and is " + _color);
         }
     }
 }

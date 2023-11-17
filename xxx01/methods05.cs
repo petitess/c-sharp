fa6b861e15ac0c41398dd3880bcfb2aa5b54321c
Karol <93782725+petitess@users.noreply.github.com>
2023-11-17 16:39:42 +0100

    Object-oriented programming OOP

--- xxx01/methods05.cs
+++ xxx01/methods05.cs
@@ -1,19 +1,34 @@
 //Program.cs
 Car audi = new Car("Audi", 400, "black");
-audi.Drive();
 audi.Stop();
 audi.Info();
-Car car = new Car();
-car.Info();
+Car volvo = new Car("Volvo");
+volvo.Info();
+//Getter, Setter
+Car bmw = new Car("BMW", 500);
+bmw.SetName("BMW X6");
+bmw.Info();
+Console.WriteLine("After chip tuning " + bmw.GetName() + " has " + bmw.GetHp() + " horsepower");
+//The public property
+Car vw = new Car("VW", 300, "white");
+vw.Name = "Volkswagen Touareg";
+vw.Info();
+//Auto implemented property
+vw.TopSpeed = 230;
+Console.WriteLine(vw.GetName() + " Top speed is " + vw.TopSpeed + " km/h");
+//ReadOnly property = get;, WriteOnly property = set;
+Console.WriteLine("After chip tuning " + vw.GetName() + " Top speed is " + vw.TopSpeedAfterChipTuning + " km/h");
 //Car.cs
 namespace exercise01
 {
     internal class Car
     {
-        //Member variable/private filed
+        //Private member variable/private filed
+        //Access Modifier private
         private string _name; 
         private int _hp;
         private string _color;
+        private int _prise;
 
         //Constractor nr1
         public Car() 
@@ -21,6 +36,7 @@ namespace exercise01
             _name = "Car";
             _hp = 0;
             _color = "red";
+            Drive();
         }
         //Constractor nr2
         public Car(string name,int hp = 0, string color = "black") 
@@ -29,9 +45,10 @@ namespace exercise01
             _hp = hp;
             _color = color;
             Console.WriteLine(name + " was created");
+            Drive();
         }
         //Member method
-        public void Drive()
+        private void Drive()
         {
             Console.WriteLine(_name + " is driving");
         }
@@ -43,6 +60,38 @@ namespace exercise01
         {
             Console.WriteLine(_name + " has " + _hp + " horsepower and is " + _color);
         }
+        //Getter
+        public int GetHp()
+        {
+            return _hp + 50;
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+        //Setter
+        public void SetName(string name)
+        {
+            if (name == "")
+            {
+                _name = "DefaultName";
+            }
+            else
+            {
+                _name = name;
+            }
+        }
+        //The public property
+        public string Name 
+        { 
+            get { return _name; } //Get accessor
+            set { _name = value; } //Set sccessor
+        }
+        //Auto implemented property
+        public int TopSpeed { get; set; }
+        //ReadOnly property = get;, WriteOnly property = set;
+        public int TopSpeedAfterChipTuning { get; } = 280;
+        public int Prise { set { _prise = value; } }
     }
 }
 ////////////

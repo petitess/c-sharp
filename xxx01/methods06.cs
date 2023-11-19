257005842b0de0c7196f88238145fdf6f00c1eeb
Karol <93782725+petitess@users.noreply.github.com>
2023-11-19 17:39:27 +0100

    Update methods06.cs

--- xxx01/methods06.cs
+++ xxx01/methods06.cs
@@ -24,3 +24,44 @@ static List<int> Solution2()
     return myList;
 }
 ////
+public static string Convert(int i)
+{
+    Dictionary<int, string> empDict = new Dictionary<int, string>()
+    {
+        {0, "zero" },
+        {1, "one" },
+        {2, "two"},
+        {3, "three" },
+        {4, "four"},
+        {5, "five" }
+    };
+    foreach (var key in empDict)
+    {
+        if (key.Key == i)
+        {
+            Console.WriteLine("Key: " + key.Key);
+            return key.Value;
+        }else
+        {
+            Console.WriteLine("nope");
+        }
+    }
+    return empDict.Keys.ToString();
+}
+/////check if an integer exists in Dictionary
+public static string Convert(int i)
+{
+    Dictionary<int, string> empDict = new Dictionary<int, string>()
+    {
+        {0, "zero" },
+        {1, "one" },
+        {2, "two"},
+        {3, "three" },
+        {4, "four"},
+        {5, "five" }
+    };
+    if (empDict.ContainsKey(i))
+        return empDict[i];
+    else
+        return "nope";
+}

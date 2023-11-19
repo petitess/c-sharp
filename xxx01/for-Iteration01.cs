44260a758426c65a38a2f6ccbe08cb8a6f45a075
Karol <93782725+petitess@users.noreply.github.com>
2023-11-19 11:48:33 +0100

    Update for-Iteration01.cs

--- xxx01/for-Iteration01.cs
+++ xxx01/for-Iteration01.cs
@@ -7,3 +7,20 @@ for (int i = 0; i < 10; i++)
         break;
     }
 }
+///Calculate average
+int[] nr = { 8, 5, 12, 8, 5, 9 };
+
+Console.WriteLine(GetAverage(nr));
+
+static double GetAverage(int[] grades)
+{
+    int size = grades.Length;
+    double average;
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum += grades[i];
+    }
+    average = (double)sum / size;
+    return average;
+}

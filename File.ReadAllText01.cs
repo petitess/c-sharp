4c6334d216e8e44a5471d8ba6fb474871940f2c1
Karol <93782725+petitess@users.noreply.github.com>
2023-11-25 09:45:04 +0100

    read file content

--- File.ReadAllText01.cs
+++ File.ReadAllText01.cs
@@ -8,3 +8,18 @@ foreach (string line in lines)
 {
     Console.WriteLine("\t" + line);
 }
+//Polymorphism
+IEnumerable<string> text = File.ReadLines(@"D:\INPUT.txt");
+string phrase = "";
+foreach (string line in text)
+{
+    if (line.Contains("split"))
+    {
+        //Console.WriteLine(line);
+        string[] subs = line.Split();
+        File.WriteAllText(@"D:\output.txt", subs[4]);
+        phrase = phrase + " " + subs[4];
+        Console.WriteLine(phrase);
+    }
+}
+//
